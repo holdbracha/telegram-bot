c5e341d55a4645aa57b8aee7d1a2c09acd7b1f21
sarabuc <sm5800810@gmail.com>
2020-11-18 12:27:26 +0200

    heroku

--- app.py
+++ app.py
@@ -1,1 +1,63 @@
-from db_pkg import *
+from flask import Flask, Response, request
+import requests
+from config import *
+from message import *
+from action import get_action
+
+try:
+    from mail import *
+except:
+    pass
+app = Flask(__name__, static_url_path='', static_folder='dist')
+req_action = None
+
+@app.route('/message', methods=["POST"])
+def handle_message():
+    print(request.get_json())
+    message = Message(request.get_json())
+    res_message = get_action(message.action, message.params)
+    res = requests.get(TELEGRAM_RES.format(TOKEN, message.chat_id, res_message))
+    return Response("success")
+
+@app.route('/sendEmailsToUser/<chat_id>', methods=["GET"])
+def send_messages_to_user(chat_id):
+    res_messages = get_action('send_emails_to_user', chat_id)
+    for message in res_messages:
+        requests.get(TELEGRAM_RES.format(TOKEN, chat_id, message))
+    return Response("success")
+
+@app.route('/check_new_mails')
+def check_new_mails(): #will calles every minute by cron job
+    return Response("success", status=200)
+
+    # addresses = get_all_mail_address()
+    # print( "line 32" , addresses)
+    # for addr in addresses:
+    #     print("line 34", addr)
+    #     chat_id, num_of_new_mails = get_mail_list_from_mail_addr(addr["address"])
+    #     print("line 36", chat_id, num_of_new_mails)
+    #     if num_of_new_mails > 0:
+    #         res_messages = get_action('send_emails_to_user', chat_id)
+    #         print("line 39", res_messages)
+    #         for message in res_messages:
+    #             requests.get(TELEGRAM_RES.format(TOKEN, chat_id, message))
+    # return Response("success")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    requests.get(TELEGRAM_INIT_WEBHOOK_URL)
+    app.run(port=3000)
