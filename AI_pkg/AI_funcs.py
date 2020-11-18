9b3bcf3c1fc7ac118446463af994c445857f136a
sarabuc <sm5800810@gmail.com>
2020-11-18 16:30:00 +0200

    heroku

---
+++ AI_pkg/AI_funcs.py
@@ -0,0 +1,7 @@
+"""
+trained model by colab,
+trainer file, csv and predict model here:
+https://drive.google.com/drive/folders/1qN4jtNZWQ4lIFoKpk9TdZg7J-JBHadhW?usp=sharing
+
+notebook src from:
+"""
